dd75cf4f88996e1f74eec51aa7c5a5352f9fa459
Nora Söderlund <nora.soderlund@polestar.com>
2023-09-05 20:59:52 +0200

    Update variables

--- dist/controllers/getInputToken.js
+++ dist/controllers/getInputToken.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@actions/core");
 function getInputToken() {
     const jiraUserEmail = (0, core_1.getInput)("JIRA_USER_EMAIL");
-    const jiraApiToken = (0, core_1.getInput)("JIRA_API_KEY");
+    const jiraApiToken = (0, core_1.getInput)("JIRA_API_TOKEN");
     return Buffer.from(`${jiraUserEmail}:${jiraApiToken}`).toString("base64url");
 }
 exports.default = getInputToken;
--- src/controllers/getInputToken.ts
+++ src/controllers/getInputToken.ts
@@ -2,7 +2,7 @@ import { getInput } from "@actions/core";
 
 export default function getInputToken() {
   const jiraUserEmail = getInput("JIRA_USER_EMAIL");
-  const jiraApiToken = getInput("JIRA_API_KEY");
+  const jiraApiToken = getInput("JIRA_API_TOKEN");
 
   return Buffer.from(`${jiraUserEmail}:${jiraApiToken}`).toString("base64url");
 };
