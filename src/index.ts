627acc8de055040ea12761ad439aacc229553de2
Nora Söderlund <nora.soderlund@polestar.com>
2023-09-05 18:43:59 +0200

    Fix imports

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
-import { getInput, setFailed, setOutput } from "@actions/core";
-import { context } from "@actions/github/lib/utils";
+import * as core from "@actions/core";
+import * as github from "@actions/github/lib/utils";
 
 try {
-  const jiraBaseUrl = getInput("jira-base-url");
-  const jiraUserEmail = getInput("jira-user-email");
-  const jiraApiToken = getInput("jira-api-token");
+  const jiraBaseUrl = core.getInput("jira-base-url");
+  const jiraUserEmail = core.getInput("jira-user-email");
+  const jiraApiToken = core.getInput("jira-api-token");
 
-  setOutput("title", "Hello world!");
+  core.setOutput("title", "Hello world!");
   
-  const payload = JSON.stringify(context.payload, undefined, 2);
-  
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
+
   console.log(`The event payload: ${payload}`);
 }
 catch(error) {
   if(error instanceof Error || typeof error === "string")
-    setFailed(error);
+    core.setFailed(error);
   else
-    setFailed("Unknown error: " + error);
+    core.setFailed("Unknown error: " + error);
 }
